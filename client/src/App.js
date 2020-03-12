779d4721c36fe5f299df43393940aa96e7726be2
Olegus <gulldan1996@gmail.com>
2020-03-12 13:00:51 +0200

    chaged browser router on history router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/createStore";
 import HomePage from "./pages_recipe/HomePage";
